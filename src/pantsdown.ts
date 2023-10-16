c44a1cbb04b8b4bc94badc50dd9d93ea6593853a
wallpants <gual.casas@gmail.com>
2023-10-16 17:00:27 -0600

    feat(slugger): setup github slugger to allow navigating to headers

--- src/pantsdown.ts
+++ src/pantsdown.ts
@@ -1,3 +1,4 @@
+import GithubSlugger from "github-slugger";
 import { Lexer } from "./lexer.ts";
 import { Parser } from "./parser.ts";
 import { type PantsdownConfig } from "./types.ts";
@@ -12,6 +13,10 @@ export class Pantsdown {
     }
 
     parse(src: string): string {
+        // re-init slugger to avoid slug count from incorrectly incrementing
+        // from previosly slugged headings
+        this.parser.renderer.slugger = new GithubSlugger();
+
         const tokens = this.lexer.lex(src);
         const html = this.parser.parse(tokens);
         return html;
