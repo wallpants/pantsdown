9c247015e04a20c3311172ee48847a61924d4feb
wallpants <gual.casas@gmail.com>
2023-10-16 23:37:45 -0600

    docs(github-page): fetch css in javascript

--- docs/index.ts
+++ docs/index.ts
@@ -4,8 +4,20 @@ const pantsdown = new Pantsdown();
 const element = document.getElementById("markdown-container");
 
 async function main() {
-    const res = await fetch("https://raw.githubusercontent.com/wallpants/pantsdown/main/README.md");
-    const markdown = await res.text();
+    // css cannot be loaded in an html script, because raw.githubusercontent.com
+    // returns a mimetype of text/plain and the browser cries
+    const cssRes = await fetch(
+        "https://raw.githubusercontent.com/wallpants/pantsdown/main/src/css/styles.css",
+    );
+    const css = await cssRes.text();
+    const styleElement = document.createElement("style");
+    styleElement.textContent = css;
+    document.head.appendChild(styleElement);
+
+    const markdownRes = await fetch(
+        "https://raw.githubusercontent.com/wallpants/pantsdown/main/README.md",
+    );
+    const markdown = await markdownRes.text();
     if (element) element.innerHTML = pantsdown.parse(markdown);
 }
 
