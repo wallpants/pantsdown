536ab225bc8e99005dc37bdcf60a465a7c2f7ec9
wallpants <gual.casas@gmail.com>
2023-10-18 01:27:24 -0600

    docs: update docs

--- docs/build.ts
+++ docs/build.ts
@@ -1,4 +1,4 @@
-// Demo build script called by github action:
+// "Pantsdown Demo" build script called by github action:
 // https://github.com/wallpants/pantsdown/blob/main/.github/workflows/github-page.yaml
 
 import { Pantsdown } from "../src";
